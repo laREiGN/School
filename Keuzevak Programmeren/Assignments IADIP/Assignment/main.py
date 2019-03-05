9af051f8e0f78cee8ac17121ca70704409491641
laREiGN / Girts <43932616+laREiGN@users.noreply.github.com>
2019-03-05 22:02:22 +0100

    assignment 2 completed

--- Keuzevak Programmeren/Assignments IADIP/Assignment/main.py
+++ Keuzevak Programmeren/Assignments IADIP/Assignment/main.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python
 
 import sys
-import functions
+import functions as f
 import types
+
+current_version = 'calculator version 0.1'
 
 '''
 The entry-point of our application.
 '''
 def main():
-    current_version = 'calculator version 0.1'
-    # Implement basic functionality
-    if input() == 'version':
+    global current_version
+    # Indicates whether or not our application should quit
+    function = input()
+    quit = False
+    if function == 'quit':
+        quit = True
+    elif quit == True:
+        return
+    elif function == 'version':
         print(current_version)
-    elif input() == 'help':
-        print('hello!')
-    elif input() == 'quit':
-        sys.exit()
-            
+        main()
+    elif function == 'help':
+        f.print_functions()
+        main()
 
 
+    # Implement basic functionality
 
-        
-  
+
 if __name__== "__main__":
     main()
