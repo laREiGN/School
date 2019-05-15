3fe9c205130d7ad227238d26d01218b7904abd3c
laREiGN / Girts <43932616+laREiGN@users.noreply.github.com>
2019-05-15 15:29:36 +0200

    yy

--- Keuzevak Programmeren/Assignments IADIP/Assignment/arith.py
+++ Keuzevak Programmeren/Assignments IADIP/Assignment/arith.py
@@ -12,13 +12,48 @@ Only supports positive integers.
 def sum(a, b):
     r = 0                                     # Result
     c = 0                                     # Carry
+    result = []
 
     length_a = arith_tools.get_number_length(a)     # Get length of number a
     length_b = arith_tools.get_number_length(b)     # Get length of number b
     max_length = max(length_a, length_b)            # Calculate maximum a if a > b, else b
     
-    # Implement algorithm
+    a1 = str(a)
+    b1 = str(b)
+    a_list = list(a1)
+    b_list = list(b1)
+    a_list.reverse()
+    b_list.reverse()
 
+    while max_length > 0:
+        if len(a_list) > 0:
+            sum1 = a_list.pop(0)
+        else:
+            sum1 = 0
+        if len(b_list) > 0:
+            sum2 = b_list.pop(0)
+        else:
+            sum2 = 0
+
+        current_step = c + int(sum1) + int(sum2)
+
+        if current_step >= 10:
+            c = 1
+            current_step = current_step - 10
+            max_length = max_length - 1
+            result.extend(str(current_step))
+        else:
+            c = 0
+            max_length = max_length - 1
+            result.extend(str(current_step))
+    if c == 1:
+        result.extend(str('1'))
+    result.reverse()
+    result_int = list(map(int, result))
+    try:
+        r = int("".join(map(str, result_int)))
+    except ValueError:
+        pass
     return r
 
 '''
@@ -29,16 +64,73 @@ Only supports positive integers.
 def sub(a, b):
     r = 0                                           # Result
     c = 0                                           # Borrow
+    result = []
 
     s = 1                                           # Sign is positive
     if b > a:
-        pass # Implement algorithm, replace pass by your own code
+        s = -1
+    elif a > b:
+        s = 1
 
     length_a = arith_tools.get_number_length(a)     # Get length of number a
     length_b = arith_tools.get_number_length(b)     # Get length of number b
     max_length = max(length_a, length_b)            # Calculate maximum a if a > b, else b
     
-    # Implement algorithm
+    a1 = str(a)
+    b1 = str(b)
+    a_list = list(a1)
+    b_list = list(b1)
+    a_list.reverse()
+    b_list.reverse()
+    if s == 1:
+        while max_length > 0:
+            if len(a_list) > 0:
+                sub1 = int(a_list.pop(0))
+            else: sub1 = 0
+            if len(b_list) > 0:
+                sub2 = int(b_list.pop(0))
+            else: sub2 = 0
+
+            if c == 1:
+                sub1 = sub1 - 1
+
+            if sub2 > sub1:
+                sub1 = sub1 + 10
+                current_step = sub1 - sub2
+                c = 1
+                max_length = max_length - 1
+                result.extend(str(current_step))
+            elif sub2 <= sub1:
+                current_step = sub1 - sub2
+                c = 0
+                result.extend(str(current_step))
+                max_length = max_length - 1
+    elif s == -1:
+        while max_length > 0:
+            if len(a_list) > 0:
+                sub2 = int(a_list.pop(0))
+            else: sub2 = 0
+            if len(b_list) > 0:
+                sub1 = int(b_list.pop(0))
+            else: sub1 = 0
+            if c == 1:
+                sub1 = sub1 - 1
+
+            if sub2 > sub1:
+                sub1 = sub1 + 10
+                current_step = sub1 - sub2
+                c = 1
+                max_length = max_length - 1
+                result.extend(str(current_step))
+            elif sub2 <= sub1:
+                current_step = sub1 - sub2
+                c = 0
+                result.extend(str(current_step))
+                max_length = max_length - 1
+
+    result.reverse()
+    result_int = list(map(int, result))
+    r = int("".join(map(str, result_int)))
 
     return r * s                                    # Note the sign
 
@@ -49,7 +141,9 @@ Only supports positive integers.
 '''
 def multiply(a, b):
     r = 0                                     # Result
-    # Implement algorithm
+    while b > 0:
+        r = sum(r, a)
+        b = b - 1
     return r
 
 '''
@@ -58,8 +152,16 @@ Calculates the division of a and b.
 Only supports positive integers.
 '''
 def divide(a, b):                       
-    i = 0                                     # Result
-    # Implement algorithm
+    i = 0
+    res = 0                                     # Result
+    if b > a:
+        i = 0
+    while a > res:
+        res = sum(res, b)
+        i = i + 1
+    if res > a:
+        i = i - 1
+
     return i
 
 '''
@@ -73,7 +175,9 @@ Only supports positive integers.
 '''
 def power(a, b):
     r = 1                                     # Result
-    # Implement algorithm
+    while b > 0:
+        r = multiply(r, a)
+        b = b - 1
     return r
 
 '''
@@ -87,10 +191,17 @@ def sqrt(a):
 
     i = 1                                           # Increments
     r = 1                                           # Result of multiplication with i
+    res = 0
     
-    # Implement algorithm
+    while res < a:
+        res = power(r,2)
+        i = i + 1
+        r = r + 1
+    if res > a:
+        i = i - 1
 
     return sub(i, 1)                                # Subtract one from increment
+
 
 '''
 Calculates the modulo of a and b.
@@ -98,7 +209,7 @@ Calculates the modulo of a and b.
 Only supports positive integers.
 '''
 def mod(a, b):
-    return sub(a, multiply(divide(a, b), b))        # Calculate a - ((a / b) * b)
+    return int(sub(a, multiply(divide(a, b), b)))        # Calculate a - ((a / b) * b)
 
 '''
 Calculates the gcd of a and b.
@@ -106,18 +217,45 @@ Calculates the gcd of a and b.
 Only supports positive integers.
 '''
 def gcd(a, b):
-    return
+    a1 = a
+    b1 = b
+    res = 1
+    b1_list = []
+    while res > 0:
+        res = mod(a1, b1)
+        a1 = b1
+        b1 = res
+        b1_list.append(res)
+    if len(b1_list) == 1:
+        return b
+    else:
+        return int(b1_list[-2])
 
+    
 '''
 Calculates the lcm of a two integers.
 
 Only supports positive integers.
 '''
 def lcm(a, b):
-    return
-
+    return divide(multiply(a, b), gcd(a, b))
 '''
 Converts a binary string to a decimal integer.
 '''
-def bin(a : str):
-    return
+def bin(a):
+    binaryNum = str(a)
+    binaryNum = list(binaryNum)
+    binaryNum.reverse()
+    steps = [1]
+    count = 1
+    amt = 0
+
+    for i in range (1,len(binaryNum)):
+        total = multiply(count, 2)
+        count = total
+        steps.append(count)
+    deep = zip(binaryNum, steps)
+    for k, v in deep:
+        if k == '1':
+            amt += v
+    return amt
--- Keuzevak Programmeren/Assignments IADIP/Assignment/main.py
+++ Keuzevak Programmeren/Assignments IADIP/Assignment/main.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python
 
 import sys
-import functions as f
 import types
+import functions as f
 
 current_version = 'calculator version 0.1'
+function = None
 
 '''
 The entry-point of our application.
 '''
+
 def main():
     global current_version
-    # Indicates whether or not our application should quit
+    global function
+    function = ''
     function = input()
+    # Indicates whether or not our application should quit
     quit = False
     if function == 'quit':
         quit = True
@@ -26,13 +30,27 @@ def main():
         main()
     else:
         process_line()
+        main()
 
 def process_line():
-    
-
-
-    # Implement basic functionality
-
+    global function
+    tokens = function.split(' ')
+    operands = []
+    for i in reversed(tokens):
+        if f.is_function(i) == True:
+            func_name = f.get_function(i)
+            if len(operands) >= 2 and (i != 'sqrt' and i != 'bin'):
+                res = func_name(operands[0], operands[1])
+                print(res)
+            elif len(operands) == 1 and (i == 'sqrt' or i == 'bin'):
+                res = func_name(operands[0])
+                print(res)
+            else:
+                print('invalid number of operands')
+        elif i.isnumeric() == True:
+            operands.insert(0, int(i))
+        else:
+            print('unknown token')
 
 if __name__== "__main__":
     main()
