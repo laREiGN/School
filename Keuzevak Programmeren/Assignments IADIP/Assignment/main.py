b43fbd036f936e8f05d515cc0aac06dc86c22b41
laREiGN / Girts <43932616+laREiGN@users.noreply.github.com>
2019-05-15 15:33:01 +0200

    Update main.py

--- Keuzevak Programmeren/Assignments IADIP/Assignment/main.py
+++ Keuzevak Programmeren/Assignments IADIP/Assignment/main.py
@@ -3,11 +3,7 @@ import types
 import functions as f
 
 current_version = 'calculator version 0.1'
-<<<<<<< HEAD
 function = input()
-=======
-function = None
->>>>>>> 3fe9c205130d7ad227238d26d01218b7904abd3c
 
 '''
 The entry-point of our application.
@@ -16,11 +12,6 @@ The entry-point of our application.
 def main():
     global current_version
     global function
-<<<<<<< HEAD
-=======
-    function = ''
-    function = input()
->>>>>>> 3fe9c205130d7ad227238d26d01218b7904abd3c
     # Indicates whether or not our application should quit
     quit = False
     if function == 'quit':
@@ -35,31 +26,6 @@ def main():
         main()
     else:
         process_line()
-<<<<<<< HEAD
-
-def process_line():
-    global function
-    tokens = function.split(' ')
-    func_name = str(tokens[0])
-    if f.is_function(func_name) == True:
-        f.get_function(func_name)
-        if func_name is not 'sqrt' or 'bin' and len(tokens) == 3:
-            opperands = int(tokens[1]), int(tokens[2])
-            print(opperands)
-        elif len(tokens) == 2 and func_name == 'sqrt' or 'bin':
-            opperands = int(tokens[1])
-            print(opperands)
-        else:
-            print('invalid operand type')
-            main()
-    else:
-        print('unknown token')
-        main()
-    
-
-
-
-=======
         main()
 
 def process_line():
@@ -81,7 +47,6 @@ def process_line():
             operands.insert(0, int(i))
         else:
             print('unknown token')
->>>>>>> 3fe9c205130d7ad227238d26d01218b7904abd3c
 
 if __name__== "__main__":
     main()
